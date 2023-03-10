636c063175d5e5d95c7c00ebc5d001006b1ecc31
Hadi <hadirezaei2828@gmail.com>
2023-03-10 13:52:24 +0330

    connected graphql to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import './index.css';
 import App from './App';
 
+const client = new ApolloClient({
+  uri: "https://api-ca-central-1.hygraph.com/v2/clf1fwcu72t9z01uj3j9becnh/master",
+  cache: new InMemoryCache()
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
 );
