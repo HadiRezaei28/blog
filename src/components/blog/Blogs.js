84020040780822a61cba196070851daa527adbf6
Hadi <hadirezaei2828@gmail.com>
2023-03-12 23:14:48 +0330

    added query for getting data about articles

--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_BLOGS_INFO } from '../../graphql/quries';
 
 const Blogs = () => {
+
+    const {loading, data, errors} = useQuery(GET_BLOGS_INFO);
+    console.log(data,loading, errors)
+
+    if(loading) return <h4>Loading...</h4>
+    if(errors) return <h4>Error...</h4>
     return (
         <div>
             Blogs
