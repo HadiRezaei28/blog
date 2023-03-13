569d69a356e3af2a46d20c95b89f0a2cc277e040
Hadi <hadirezaei2828@gmail.com>
2023-03-13 11:28:00 +0330

    Sorted the blogs section and making it responsive

--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { GET_BLOGS_INFO } from '../../graphql/quries';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_BLOGS_INFO } from "../../graphql/quries";
+import { Grid } from "@mui/material";
+import CardEL from "../shared/CardEL";
 
 const Blogs = () => {
+  const { loading, data, errors } = useQuery(GET_BLOGS_INFO);
+  console.log(data, loading, errors);
 
-    const {loading, data, errors} = useQuery(GET_BLOGS_INFO);
-    console.log(data,loading, errors)
-
-    if(loading) return <h4>Loading...</h4>
-    if(errors) return <h4>Error...</h4>
-    return (
-        <div>
-            Blogs
-        </div>
-    );
+  if (loading) return <h4>Loading...</h4>;
+  if (errors) return <h4>Error...</h4>;
+  return (
+    <Grid container spacing={2}>
+      {data.posts.map((post) => (
+        <Grid item xs={12} sm={6} md={4} key={post.id}>
+            <CardEL {...post}/>
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default Blogs;
---
+++ src/components/shared/CardEL.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardEL = () => {
+    return (
+        <div>
+            CardEL
+        </div>
+    );
+};
+
+export default CardEL;
