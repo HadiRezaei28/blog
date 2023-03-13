7a979d116b7aecfa098337170703a7c48592bd22
Hadi <hadirezaei2828@gmail.com>
2023-03-13 18:34:47 +0330

    added query for get data Blog using slug

--- src/components/blog/BlogPage.js
+++ src/components/blog/BlogPage.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import { useQuery } from "@apollo/client";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { GET_BLOG_INFO } from "../../graphql/quries";
+import Loader from "../shared/Loader";
 
 const BlogPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { slug } = useParams();
+  const { loading, data, errors } = useQuery(GET_BLOG_INFO, {
+    variables: { slug },
+  });
+
+  if(loading) return <Loader />
+  if(errors) return <h3>Errors...</h3>
+  
+  return <div></div>;
 };
 
 export default BlogPage;
