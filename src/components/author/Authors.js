d282411bedbb273233813af2b391d545dd1c92d1
Hadi <hadirezaei2828@gmail.com>
2023-03-13 12:29:02 +0330

    added query for getting data about authors

--- src/components/author/Authors.js
+++ src/components/author/Authors.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_AUTHORS_INFO } from '../../graphql/quries';
 
 const Authors = () => {
+    const {loading, data, errors} = useQuery(GET_AUTHORS_INFO);
+
+    if(loading) return <h3>Loading...</h3>
+    if(errors) return <h3>Error...</h3>
     return (
         <div>
             Authors
